080a33877b01f43b72c8175693b280034451720d
Shivam Sukhwal <49242766+ShivamSukhwal@users.noreply.github.com>
2020-04-09 12:52:48 -0400

    game fully functional

--- tic-tac-toe/src/components/Game.js
+++ tic-tac-toe/src/components/Game.js
@@ -2,8 +2,15 @@ import React, { useState } from "react";
 import Board from "./Board";
 import { calculateWinner } from "../Helper";
 
+const style = {
+  width: "250px",
+  margin: "0 auto",
+};
+
 const Game = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
+  const BOARD_VALUE = Array(9).fill(null);
+
+  const [board, setBoard] = useState(BOARD_VALUE);
   const [xIsNext, setXisNext] = useState("true");
   const winner = calculateWinner(board);
 
@@ -15,7 +22,22 @@ const Game = () => {
     setXisNext(!xIsNext);
   };
 
-  return <Board squares={board} onClick={handleClick} />;
+  const startGame = () => {
+    return <button onClick={() => setBoard(BOARD_VALUE)}>Start Game</button>;
+  };
+
+  return (
+    <>
+      <Board squares={board} onClick={handleClick} />
+      <p style={style}>
+        {winner
+          ? "Winner is " + winner
+          : "Next player: " + (xIsNext ? "X" : "O")}
+        <br />
+        {startGame()}
+      </p>
+    </>
+  );
 };
 
 export default Game;
