95797a6e39d95410d27d10e1aa521bd1b256479e
Shivam Sukhwal <49242766+ShivamSukhwal@users.noreply.github.com>
2020-04-09 12:40:07 -0400

    set style for square and board + made handleClick function

--- tic-tac-toe/src/components/Game.js
+++ tic-tac-toe/src/components/Game.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Board from "./Board";
+import { calculateWinner } from "../Helper";
 
 const Game = () => {
-  const handleClick = () => {};
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [xIsNext, setXisNext] = useState("true");
+  const winner = calculateWinner(board);
 
-  return <Board onClick={handleClick} />;
+  const handleClick = (i) => {
+    const gameBoard = [...board];
+    if (winner || gameBoard[i]) return;
+    gameBoard[i] = xIsNext ? "X" : "O";
+    setBoard(gameBoard);
+    setXisNext(!xIsNext);
+  };
+
+  return <Board squares={board} onClick={handleClick} />;
 };
 
 export default Game;
